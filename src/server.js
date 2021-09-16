d07bd7e748d5a7d2e6c439974d002be7d317b755
wook95 <uclee95@gmail.com>
2021-09-16 21:00:25 +0900

    Add: mission 5 완료 - 프리즈마를 이용한 DB 조회및 삽입

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 dotenv.config();
 const app = express();
@@ -10,6 +13,73 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+app.get('/categories', async (req, res) => {
+  const category = await prisma.$queryRaw`
+    SELECT c.id, c.name
+    FROM categories as c;
+  `;
+
+  res.json(category);
+});
+
+app.post('/categories/new-categories', async (req, res) => {
+  await prisma.$queryRaw`
+    INSERT INTO categories (name)
+    VALUES ("클램차우더");
+  `;
+
+  const [category] = await prisma.$queryRaw`
+    SELECT c.id , c.name
+    FROM categories as c
+    ORDER BY id DESC
+    LIMIT 1;
+  `;
+
+  res.json(category);
+});
+
+app.get('/products', async (req, res) => {
+  const product = await prisma.$queryRaw`
+  SELECT p.id, p.korean_name, p.english_name, p.category_id
+  FROM products as p;
+  `;
+
+  res.json(product);
+});
+
+app.get('/products/:id', async (req, res) => {
+  let id = req.params.id;
+
+  const product = await prisma.$queryRaw`
+  SELECT p.id, p.korean_name, p.english_name, p.category_id
+  FROM products as p
+  where p.id=${id};
+  `;
+
+  res.json(product);
+});
+
+app.post('/user/new-user', async (req, res) => {
+  await prisma.$queryRaw`
+  INSERT INTO users (email,password,username,address,phone_number,policy_agreed)
+  VALUES ("uclee95@wecode.conmm","0905","wook","gwangmeyong","01072298999","1"),
+  ("eebzaaa@wecode.conmm","0905","yebom","busan","0101238999","1"),  
+  ("fireking5997@wecode.conmm","0905","park","종로타워지킴이","01039050101","1"),
+  ("dabin0219@wecode.conmm","0905","dabin","구파발헬창","01071799766","1"),
+  ("changh950@wecode.cnomm","0905","chang","선릉2호점지킴이","01026054161","1"),
+  ("wjdghtls11@wecode.cnomm","0905","ho","양천구교육열의도시","01027563617","1");  
+  `;
+
+  const user = await prisma.$queryRaw`
+  SELECT *
+  FROM users
+  ORDER BY id DESC
+  LIMIT 6;
+  `;
+
+  res.json(user);
 });
 
 app.listen(PORT, () => console.log(`server on ${PORT}`));
